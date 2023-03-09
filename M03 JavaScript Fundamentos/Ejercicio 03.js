b515abbe38011a32453200cda72d29a76c30fdb8
Jwohnsiedler <tradingforexbots@gmail.com>
2023-03-08 21:01:16 -0500

    Ejercicios 03.js HECHO

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,40 +5,50 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
-   var x = "x";
-   var y = "x";
-   return (x == y);
+   if (x === y){return true;}
+   else {return false;}
 }
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
-
+   if (str1.length === str2.length) {return true;}
+   else {return false;}
 }
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
+   var num0 = 90
+   if (num < num0){return true;}
+   else {return false;}
 }
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
+   var num0 = 50
+   if (num > num0){return true;}
+   else {return false;}
 }
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
+   if ((num % 2) === 0){return true;}
+   else {return false;}
 }
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+   if ((num % 2) === 0){return false;}
+   else {return true;}
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
